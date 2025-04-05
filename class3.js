5865eb9142633a74f8d501806b85db795b3c550c
Amisha1172 <amishasah223@gmail.com>
2025-04-05 15:04:34 +0545

    added function

---
+++ class3.js
@@ -0,0 +1,39 @@
+// function sayMyName() {
+//     console.log("Love Bubber");
+    
+// }
+
+// // // function used - function call
+// // sayMyName();
+
+
+// function printCounting() {
+//     for(let i=1; i<=100; i++){
+//    console.log(i);
+   
+//     }
+// }
+// printCounting(); 
+
+// function PrintNumber(num) {
+// console.log("printing Number:",num);
+
+// }
+// PrintNumber(4);
+
+// function getAverage(num1,num2){
+//     let avg = (num1+num2)/2
+//     console.log("Average",avg);
+// }
+
+// getAverage(3,70);
+
+// Return Function
+
+function getSum(a,b,c) {
+let sum = a+b+c;
+return sum;
+}
+   let ans = getSum(1,2,3);
+   console.log("printing sum:", ans);
+   
