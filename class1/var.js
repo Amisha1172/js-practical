349d2f445919e93477733000dbf90c1704226301
Amisha1172 <amishasah223@gmail.com>
2025-04-10 12:32:56 +0545

    added var

---
+++ class1/var.js
@@ -0,0 +1,21 @@
+//   let a = 10;
+//   console.log(a);
+
+// creat let var use
+
+// const a = 25;
+// console.log(a);
+// a = 25;
+// console.log(a); 
+
+// let marks = 25;
+// console.log(25.022);
+
+let value =  null;
+// console.log(marks);
+
+
+
+
+
+  
